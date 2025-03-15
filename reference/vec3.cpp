0da5502f66b65134987a0f8d8e52131edac66bed
Aniket Patel <ampatel@ualbeta.ca>
2025-03-15 17:06:30 -0600

    Finished

--- reference/vec3.cpp
+++ reference/vec3.cpp
@@ -5,8 +5,8 @@ double &vec3::x() { return this->v[0]; }
 double &vec3::y() { return this->v[1]; }
 double &vec3::z() { return this->v[2]; }
 
-vec3 vec3::operator-() {
-    return vec3(-this->v[0], -this->v[1], -this->v[2]);
+vec3 vec3::operator-() const {
+    return vec3(-v[0], -v[1], -v[2]);
 }
 
 vec3 &vec3::operator+=(vec3 &rhs) {
@@ -112,6 +112,13 @@ vec3 cross(const vec3 &_u, const vec3 &_v) {
 vec3 unit_vector(const vec3 &_v) {
     return _v / _v.length();
 }
+vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
 vec3 random_unit_vector() {
     while (true) {
         auto p = vec3::random(-1, 1);
@@ -130,6 +137,12 @@ vec3 random_on_hemisphere(const vec3 &normal) {
 }
 vec3 reflect(const vec3 &v, const vec3 &n) {
     return v - 2 * dot(v, n) * n;
+}
+vec3 refract(const vec3 &uv, const vec3 &n, double etai_over_etat) {
+    auto cos_theta = std::fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+    vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
 }
 
 std::ostream &operator<<(std::ostream &os, const vec3 &_v) {
